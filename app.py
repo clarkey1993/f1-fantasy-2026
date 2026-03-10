683535df30240e69d76acf803d043b7edb2de0d4
clarkey1993 <aaronclarketech@gmail.com>
2026-03-10 07:06:44 +0100

    gunicord port

--- app.py
+++ app.py
@@ -899,4 +899,5 @@ def admin_sync():
     return redirect(url_for('admin'))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
